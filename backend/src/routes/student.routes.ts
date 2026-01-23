e8315dc38456382b01e1fac386e9ef418875121b
Nathan Araújo <nathan.marques176@gmail.com>
2026-01-23 16:38:58 -0300

    cleanup

--- backend/src/routes/student.routes.ts
+++ backend/src/routes/student.routes.ts
@@ -4,10 +4,8 @@ import { StudentController } from "../controllers/student.controller";
 const router = Router();
 const studentController = new StudentController();
 
-router.post("/", studentController.create.bind(studentController));
 router.get("/search", studentController.search.bind(studentController));
 router.get("/", studentController.getAll.bind(studentController));
 router.get("/random", studentController.getRandom.bind(studentController));
-router.put("/:id", studentController.update.bind(studentController));
 
 export const studentRouter = router;
